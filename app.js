a052f52d643143454eb5ee90a8e60c43a08814b7
chrisngnstpls <chrisngnstpls@gmail.com>
2022-07-26 11:37:46 +0300

    cors

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.use(express.static('public'))
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", '0.0.0.0');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin, X-Requested, Content-Type, Accept Authorization,application/json');
     next();
 });
+
+
 app.get('/', (req,res)=>{
     res.render('index')
 })
